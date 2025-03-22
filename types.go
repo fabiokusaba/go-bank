578444085b372568fac34f637e0363e25d9604d7
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-21 22:44:39 -0300

    removed ID to NewAccount func

--- types.go
+++ types.go
@@ -23,7 +23,6 @@ type Account struct {
 // Criando uma nova instância de Account
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(10000),
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(1000000)),
