6147856e49c6049bf4a018f8f864eff37b6ebfdf
Fabio Kusaba <fabinhokb44@gmail.com>
2025-03-23 17:27:46 -0300

    implemented seed functions

--- main.go
+++ main.go
@@ -6,6 +6,23 @@ import (
 	"log"
 )
 
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "anthony", "gg", "hunter123")
+}
+
 func main() {
 	seed := flag.Bool("seed", false, "seed the db")
 	flag.Parse()
